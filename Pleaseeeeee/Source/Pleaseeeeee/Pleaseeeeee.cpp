fd317dee5142ff27cf35a32f8994029e325be725
ggoma <ggoma8060@gmail.com>
2024-04-17 15:54:56 +0900

    please

---
+++ Pleaseeeeee/Source/Pleaseeeeee/Pleaseeeeee.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pleaseeeeee.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pleaseeeeee, "Pleaseeeeee" );
+ 
---
+++ Pleaseeeeee/Source/Pleaseeeeee/PleaseeeeeeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PleaseeeeeeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APleaseeeeeeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APleaseeeeeeGameMode();
+};
+
+
+
